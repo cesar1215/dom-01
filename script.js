372ddc3e1791c3324ae00ce7cd59d6787f1d83f4
cesar1215 <cenriquez@angloaleman.edu.mx>
2022-10-31 13:32:37 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//Cambia todo el contenido del body con innerHTML por ="Programar es divertido"
+document.body.innerText = 'Programar es super divertido.';
+
+//Utiliza innerHTML para agregarle una etiqueta h1 que contenga = "Yo se programar en Javascript
+document.body.innerHTML = "<h1>Yo se programar en Javascript</h1>";
+
+//Utiliza innerHTML para agregar una etiqueta p que contenga = "Ahoya ya se programar en el DOM"
+document.body.innerHTML= "<p>Ya se programar en el DOM</p>"
